b9ff3262c625487f9cbebb95ef14ad0b08e36b2c
yuanwentao <yuanwentao01@corp.netease.com>
2026-06-18 20:05:13 +0800

    feat(core): workspace + loomgui_core 骨架

---
+++ loomgui_core/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn version() -> &'static str {
+    "v0-skeleton"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn version_is_set() {
+        assert_eq!(version(), "v0-skeleton");
+    }
+}
